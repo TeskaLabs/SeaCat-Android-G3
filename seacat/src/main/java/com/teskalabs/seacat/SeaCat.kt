7f69b99ac31910f7885690ba329d11ba5925f904
Ales Teska <ales.teska@teskalabs.com>
2019-06-05 13:53:08 +0200

    Merge branch 'master' of github.com:TeskaLabs/SeaCat-Android-G3

--- seacat/src/main/java/com/teskalabs/seacat/SeaCat.kt
+++ seacat/src/main/java/com/teskalabs/seacat/SeaCat.kt
@@ -7,8 +7,6 @@ import java.util.concurrent.Executors
 class SeaCat(val context: Context, val title: String, val apiURL: String) {
 
     companion object {
-//        val getURL = "https://keyote.teskalabs.com/get"
-//        var enrollURL = "https://keyote.teskalabs.com/enroll"
         val certificateFactory = CertificateFactory.getInstance("X.509")
         val executor = Executors.newSingleThreadScheduledExecutor()
     }

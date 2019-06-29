5f1de80747e6f887225d2556776dfdb3e8a72c0f
Ales Teska <ales.teska@teskalabs.com>
2019-06-29 21:19:27 +0200

    Add a default logging tag.

--- seacat/src/main/java/com/teskalabs/seacat/SeaCat.kt
+++ seacat/src/main/java/com/teskalabs/seacat/SeaCat.kt
@@ -11,6 +11,8 @@ class SeaCat(internal val context: Context, internal val title: String, internal
         internal val certificateFactory = CertificateFactory.getInstance("X.509")
         internal val executor = Executors.newSingleThreadScheduledExecutor()
 
+        val TAG = "SeaCat"
+
         const val CATEGORY_SEACAT = "com.teskalabs.seacat.intent.category.SEACAT"
         const val ACTION_IDENTITY_ESTABLISHED = "com.teskalabs.seacat.intent.action.IDENTITY_ESTABLISHED"
         const val ACTION_IDENTITY_RESET = "com.teskalabs.seacat.intent.action.IDENTITY_RESET"

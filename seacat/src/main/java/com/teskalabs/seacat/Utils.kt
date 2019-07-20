f56ae13cc1641a248fb64038a7f71f71f04621cf
Ales Teska <ales.teska@teskalabs.com>
2019-07-20 10:49:06 +0200

    The DeviceUUID based on ANDROID_ID is re-enabled.

--- seacat/src/main/java/com/teskalabs/seacat/Utils.kt
+++ seacat/src/main/java/com/teskalabs/seacat/Utils.kt
@@ -2,6 +2,7 @@ package com.teskalabs.seacat
 
 import android.content.Context
 import android.os.Build
+import android.provider.Settings
 import android.util.Log
 import java.util.*
 
@@ -24,11 +25,12 @@ fun isEmulator(): Boolean {
 // Produce a device/user unique identifier (non-cryptographic strength)
 fun getUniqueIdentifier(context: Context): String {
     var deviceUuid: UUID? = null
-//    val androidId = Secure.getString(context.getApplicationContext().getContentResolver(), Secure.ANDROID_ID)
-//
-//    if ((androidId != null) && (androidId != "9774d56d682e549c")) {
-//        deviceUuid = UUID.nameUUIDFromBytes(androidId.toByteArray(charset("utf8")))
-//    }
+
+    // This part is actually optional and could be commented out if e.g. Google restricts usage of ANDROID_ID
+    val androidId = Settings.Secure.getString(context.getApplicationContext().getContentResolver(), Settings.Secure.ANDROID_ID)
+    if ((androidId != null) && (androidId != "9774d56d682e549c")) {
+        deviceUuid = UUID.nameUUIDFromBytes(androidId.toByteArray(charset("utf8")))
+    }
 
     if (deviceUuid == null) {
         val prefs = context.getSharedPreferences("com.teskalabs.seacat", Context.MODE_PRIVATE)
